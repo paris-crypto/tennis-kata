748ee91081a023026dbea534823bae90a559f000
paris-crypto <omarmejri@live.fr>
2019-12-08 22:34:41 +0100

    User story 2 sprint 1

--- src/main/java/com/tennis/kata/TennisGame1.java
+++ src/main/java/com/tennis/kata/TennisGame1.java
@@ -35,13 +35,18 @@ public class TennisGame1 implements TennisGame {
                 case 2:
                         score = "Thirty-All";
                     break;
-                
+                default:
+                    score = "Deuce";
+                    break;
             }
         }
+
         else if (m_score1>=4 || m_score2>=4)
         {
             int minusResult = m_score1-m_score2;
-            if (minusResult>=2) score = "Win for player1";
+            if (minusResult==1) score ="Advantage player1";
+            else if (minusResult ==-1) score ="Advantage player2";
+            else if (minusResult>=2) score = "Win for player1";
             else score ="Win for player2";
         }
         else
